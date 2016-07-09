e776d333df0fb7b9974f06be683d03044bb97cf2
Ariel M Shaked <mtr131@gmail.com>
2016-07-09 19:41:33 +0100

    Disabled window resizing.

--- SDVCropValueCalculator/Form1.Designer.cs
+++ SDVCropValueCalculator/Form1.Designer.cs
@@ -287,6 +287,7 @@ namespace SDVCropValueCalculator
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(580, 356);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
